859222208f075d003c347f4996f428735857cee5
Jordan West <jordwest@gmail.com>
2021-06-06 17:58:03 +1000

    basic wat to wasm compilation and instantiation

---
+++ playground/README.md
@@ -0,0 +1,10 @@
+# WES-80 Playground
+
+This is the main frontend for users to experiment with WES-80, allowing live editing of WebAssembly code with an instance of WES-80 running the code alongside it.
+
+## Building for development
+
+```
+npm install
+npm run dev
+```
--- playground/src/main.ts
+++ playground/src/main.ts
@@ -1,8 +1,62 @@
 import './style.css'
+import {editor, languages} from 'monaco-editor';
+import wabt from 'wabt';
 
-const app = document.querySelector<HTMLDivElement>('#app')!
+const editorDiv = document.querySelector<HTMLDivElement>('#editor');
+const consoleContainer = document.querySelector<HTMLDivElement>('#console-container');
+const sizeStatusContainer = document.querySelector<HTMLDivElement>('#size-status');
 
-app.innerHTML = `
-  <h1>Hello Vite!</h1>
-  <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
-`
+const defaultModule = `(module
+  (func $add (param $lhs i32) (param $rhs i32) (result i32)
+    local.get $lhs
+    local.get $rhs
+    i32.add)
+  (export "add" (func $add))
+  (export "add_again" (func $add))
+)`
+
+let codeEditor: editor.IStandaloneCodeEditor | undefined;
+
+const start = () => {
+    if (editorDiv == null) {
+        throw new Error('#editor element not found');
+    }
+    
+    codeEditor = editor.create(editorDiv, {
+        value: defaultModule,
+        language: 'wat',
+        minimap: {
+            enabled: false
+        }
+    });
+}
+
+const maxBytes = 80 * 1024;
+
+const buildAndRun = async () => {
+    if(codeEditor == null) throw new Error('Code editor not found');
+
+    const wabtModule = await wabt();
+
+    let wasmModule;
+    try {
+        wasmModule = wabtModule.parseWat("wes80.wat", codeEditor.getValue());
+    } catch (e) {
+        alert(e);
+        console.error(e);
+        return;
+    }
+
+    const wasm = wasmModule.toBinary({log: true});
+    const instantiatedModule = await WebAssembly.instantiate(wasm.buffer);
+    const bytes = wasm.buffer.length;
+    
+    sizeStatusContainer!.textContent = `${bytes} bytes (${Math.round(bytes/maxBytes * 10000) / 100}% full)`
+    
+    const exportedNames = Object.keys(instantiatedModule.instance.exports);
+    consoleContainer!.innerHTML = "This doesn't actually run anything yet, but here's a list of exports from the compiled module<h4>Exports</h4><ul>" + exportedNames.map(e => `<li>${e} - ${instantiatedModule.instance.exports[e]}</li>`).join('') + '</ul>'
+    
+}
+
+start()
+document.querySelector('#build-and-run')!.addEventListener('click', buildAndRun);
